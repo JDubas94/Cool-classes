8503527c453c9250b12abda3f77289ffd3c576e7
Julia Dubas <uliadubas94@gmail.com>
2022-07-25 13:27:51 +0300

    Add project files.

---
+++ Cool classes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cool_classes
+{
+    class Person
+    {
+        private string _name;
+        private string _age;
+        public Person(string name, string age)
+        {
+            _name = name;
+            _age = age;
+
+        }
+
+        public void GetInfo()
+        {
+             Console.WriteLine(_name + " age is " + _age);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            var name1 = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+
+            var age1 = Console.ReadLine();
+
+            Person person = new Person(name1, age1);
+
+            person.GetInfo();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
